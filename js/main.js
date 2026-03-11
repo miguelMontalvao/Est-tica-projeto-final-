e70d98b8ce343895a7a34b30e4308705695cfd8d
miguelMontalvao <miguel.m.montalvao@gmail.com>
2026-03-11 01:19:11 -0300

    atualizado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-const coelho = document.querySelector('#coelho');
-const aparece_coelho = document.querySelector('.coelho');
 
-coelho.addEventListener('click', function() {
-    aparece_coelho.classList.add('visivel');
-
-})
